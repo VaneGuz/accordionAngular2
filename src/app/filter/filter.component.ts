777a76604bceeee8b1f21383c0faf87c2fa557c5
Vanesa Guzmán <vanesa.guzman@udea.edu.co>
2017-10-11 22:22:47 -0500

    RadioButton ready

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -7,5 +7,21 @@ import { Component } from '@angular/core';
 })
 export class FilterComponent {
   title = 'Filter Component';
+  estadoProceso = {
+    snProgramado: false,
+    snEjecutado: false
+  };
+  constructor() {
+    this.estadoProceso.snEjecutado = true;
+  }
 
+  onChangeEstadoProceso(estado: number) {
+    if (estado === 1) {
+      this.estadoProceso.snEjecutado = true;
+      this.estadoProceso.snProgramado = false;
+    } else if (estado === 2) {
+      this.estadoProceso.snEjecutado = false;
+      this.estadoProceso.snProgramado = true;
+    }
+  }
 }
